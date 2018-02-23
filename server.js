98a5e884e64c29fc064d972185bc25969f66e179
unknown <matthew.fallucca@gmail.com>
2018-02-23 11:20:19 -0600

    modifying for mongolab

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static("client/build"));
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://admin:password>@ds147668.mlab.com:47668/heroku_txsfzn08",
+  process.env.MONGOLAB_URI || "mongodb://admin:password>@ds147668.mlab.com:47668/heroku_txsfzn08",
   {
     useMongoClient: true
   }
